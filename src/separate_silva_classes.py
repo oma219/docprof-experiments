b423776519d2a1dc38d0fb42cb14cfe7c9a59268
oma219 <omaryfekry@gmail.com>
2023-01-16 18:00:27 -0500

    added some code to parse SILVA taxonomy to get genera

--- src/separate_silva_classes.py
+++ src/separate_silva_classes.py
@@ -11,8 +11,207 @@ import os
 import sys
 import argparse
 
+class TreeNode(object):
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+
+    def add_child(self, obj):
+        self.children.append(obj)
+
+def validate_subtree(tree_str):
+  assert tree_str.count("(") == tree_str.count(")")
+
+def strip_parentheses(tree_str):
+  return tree_str[1:-1]
+
+def node_split(input_str):
+  """ 
+  Split str based on comma but go around interior nodes so 
+  ((One,Two),(Three,Four)),Five would return a list of 
+  [((One,Two),(Three,Four)), Five]
+  """
+  out_list = []; pos = 0
+  while pos < len(input_str):
+    curr_str = ""
+    # Case 1: interior node 
+    if input_str[pos] == "(":
+      curr_str += input_str[pos]
+      status = 1
+      pos += 1
+
+      while status != 0:
+        if input_str[pos] == "(":
+          status += 1
+        elif input_str[pos] == ")":
+          status -= 1
+        curr_str += input_str[pos]
+        pos += 1
+      out_list.append(curr_str)
+    # Case 2: comma
+    elif input_str[pos] == ",":
+      pos += 1
+    # Case 3: tip
+    else:
+      while pos < len(input_str) and input_str[pos] != ",":
+        curr_str += input_str[pos]
+        pos += 1
+      out_list.append(curr_str)
+  return out_list
+
+def find_max_depth_of_tree(curr_node):
+  """ return depth of a tree"""
+  if len(curr_node.children) == 0:
+    return 0
+  else:
+    max_depth = 0
+    for child in curr_node.children:
+      max_depth = max(max_depth, find_max_depth_of_tree(child))
+    return (max_depth+1)
+
+def traversal_newick_tree(curr_node, traversal):
+  """ performs a pre-order traversal of tree """
+  traversal.append(curr_node.data)
+  if len(curr_node.children) == 0:
+    return traversal
+  else:
+    for child in curr_node.children:
+      traversal = traversal_newick_tree(child, traversal)
+  return traversal
+
+def find_nodes_at_depth_N(curr_node, N, node_list, curr_depth):
+  if curr_depth == N:
+    node_list.append(curr_node.data)
+    return node_list
+  else:
+      for child in curr_node.children:
+        node_list = find_nodes_at_depth_N(child, N, node_list, curr_depth+1)
+      return node_list
+
+def get_leaf_nodes_of_tree(curr_node, node_list):
+  if len(curr_node.children) == 0:
+    node_list.append(curr_node.data)
+    return node_list
+  else:
+    for child in curr_node.children:
+      node_list = get_leaf_nodes_of_tree(child, node_list)
+    return node_list
+
+def build_newick_tree(curr_node, tree_str):
+  """ builds the tree based on newick format """
+  validate_subtree(tree_str)
+  if "(" not in tree_str and ")" not in tree_str:
+    children = tree_str.split(",")
+    for child in children:
+      curr_node.add_child(TreeNode(child))
+  else:
+    interior_children = node_split(tree_str)
+    for child in interior_children:
+      if "(" not in child:
+        curr_node.add_child(TreeNode(child))
+      else:
+        curr_node.add_child(build_newick_tree(TreeNode("Interior"), strip_parentheses(child)))
+  return curr_node
+
+def build_ordered_genera_list(traversal_with_names, genera_list):
+  """ Based on the traversal, reorganize the genera list """
+  final_list = []
+  for taxa in traversal_with_names:
+    if taxa is not "Interior" and taxa is not "Root":
+      if taxa in genera_list and taxa not in final_list:
+        final_list.append(taxa)
+  assert len(final_list) == len(genera_list)
+  return final_list
+
+def get_silva_tree(input_filepath):
+    """ Loads the input in Newick Tree format """
+    in_fd = open(input_filepath, "r")
+    lines = [x.strip() for x in in_fd.readlines()]
+    assert len(lines) == 1
+
+    silva_tree_str = lines[0][:-2]
+    return silva_tree_str
+
+def load_id_to_name_map(input_filepath):
+  """ Takes in map file to connect ID numbers to taxa names """
+  in_fd = open(input_filepath, "r")
+  lines = [x.strip() for x in in_fd.readlines()]
+
+  id_map = {}
+  for line in lines:
+    line_split = line.split()
+    # build the name, since some are multiple words
+    name = line_split[1]; pos = 2
+    while line_split[pos] != "-1":
+      name += " " + line_split[pos]
+      pos += 1
+    id_map[int(line_split[0])] = name
+  return id_map
+
+def load_silva_headers(silva_filepath):
+  """ Loads the SILVA database into memory """
+  in_fd = open(silva_filepath, "r")
+  all_lines = [x.strip() for x in in_fd.readlines() if ">" in x]
+  return all_lines
+
+def get_genera_list(silva_seqs):
+  """ Take in a list of headers, and return all the genera """
+  genera_list = set()
+  eukar_list = set()
+  for header in silva_seqs:
+    # 1. For Bacteria/Archaea, we focus on sequences with 6 taxa levels
+    if "Archaea;" in header or "Bacteria;" in header:
+      header_split = header.split(";")
+      if len(header_split) == 7 and header_split[5] != "uncultured":
+        genera_list.add(header_split[5])
+    # 2. For Eukaryota, we focused on sequences where genus is in organism name
+    elif "Eukaryota" in header:
+        header_split = header.split(";")
+        if header_split[-2] in header_split[-1] and header_split[-2] != "uncultured":
+          genera_list.add(header_split[-2])
+  return list(genera_list)
+
+def main_use_order(args):
+    """ main method, version that uses tree order to organize documents """
+
+    # Step 1: Load in SILVA tree structure, determine a list of the genera
+    #         ordered based on the tree structure.
+    full_input_tree = get_silva_tree(args.tree_path)
+    id_map = load_id_to_name_map(args.tree_map_path)
+    silva_seqs = load_silva_headers(args.input_file)
+    genera_list = get_genera_list(silva_seqs)
+
+    # Build the SILVA tree, perform traversal, and generate ordered list
+    root = build_newick_tree(TreeNode("Root"), strip_parentheses(full_input_tree))
+
+    traversal = traversal_newick_tree(root, [])
+    traversal_with_names = [id_map[int(x)] if x is not "Interior" and x is not "Root" else x for x in traversal]
+    ordered_genera_list = build_ordered_genera_list(traversal_with_names, genera_list)
+
+    # Step 2: Process the headers to identify how many sequences are in each genera
+    count_dict = dict.fromkeys(ordered_genera_list, 0)
+    zero_count = 0
+    for header in silva_seqs:
+        # 1. For Bacteria/Archaea, we focus on sequences with 6 taxa levels
+        if "Archaea;" in header or "Bacteria;" in header:
+            header_split = header.split(";")
+            if len(header_split) == 7 and header_split[5] != "uncultured":
+                count_dict[header_split[5]] += 1
+        # 2. For Eukaryota, we focused on sequences where genus is in organism name
+        elif "Eukaryota" in header:
+            header_split = header.split(";")
+            if header_split[-2] in header_split[-1] and header_split[-2] != "uncultured":
+                count_dict[header_split[-2]] += 1
+
+    # with open(args.output_dir + "output.csv", "w") as out_fd:
+    #     for i, genus in enumerate(ordered_genera_list):
+    #         out_fd.write(f"{i+1},{count_dict[genus]}\n")
+
+    # Step 3: Write out sequences for each genera separately
+
+
 def main(args):
-    """ main method of the script """
+    """ main method of the script (original version, does not use tree order)"""
     
     # Step 1: Extract the lines from the SILVA database
     all_lines = []
@@ -65,14 +264,15 @@ def main(args):
         for i in range(5000):
             out_fd.write(f"{args.output_dir}tax_group_{i+1}.fna {i+1}\n")
 
-
-
 def parse_arguments():
     """ Defines the command-line argument parser, and return arguments """
     parser = argparse.ArgumentParser(description="This script allows users to separate a SILVA database based on a "
                                                   "specific level of the taxonomy. ")
     parser.add_argument("-i", "--input", dest="input_file", required=True, help="input SILVA database")
     parser.add_argument("-o", "--output-dir", dest="output_dir", required=True, help="output directory for files")
+    parser.add_argument("--use-order", dest="use_genera_order", action="store_true", default=False, help="use SILVA tree order to order the documents")
+    parser.add_argument("--tree", dest="tree_path", default="", required=False, help="path to *.tre file for the SILVA database")
+    parser.add_argument("--tree-map", dest="tree_map_path", default="", required=False, help="path to *.map file for the SILVA database")
     args = parser.parse_args()
     return args
 
@@ -86,8 +286,26 @@ def check_arguments(args):
         exit(1)
     elif args.output_dir[-1] != '/':
         args.output_dir += "/"
+    
+    if args.use_genera_order:
+        if not os.path.isfile(args.tree_path):
+            print("Error: path to *.tre file is not valid")
+            exit(1)
+        if not os.path.isfile(args.tree_map_path):
+            print("Error: path to *.map file is not valid")
+            exit(1)
+        
+        if ".tre" not in args.tree_path:
+            print("Error: the tree file does not have valid extension (*.tre)")
+            exit(1)
+        if ".map" not in args.tree_map_path:
+            print("Error: the tree map file does not have valid extension (*.map)")
+            exit(1)
 
 if __name__ == "__main__":
     args = parse_arguments()
     check_arguments(args)
-    main(args)
+    if not args.use_genera_order:
+        main(args)
+    else:
+        main_use_order(args)
